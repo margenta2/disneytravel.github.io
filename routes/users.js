e2662118cf4288f62a5fc966245e4aaf34965bfa
Margie Ball <margie_ball@glic.com>
2021-05-25 19:47:50 -0400

    built out user router

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers');
+
+router.get('/', ctrl.users.index);
+router.get('/signup', ctrl.users.renderSignup);
+router.get('/profile/:index', ctrl.users.renderProfile);
+router.get('/login', ctrl.users.renderLogin);
+
+router.post('/', ctrl.users.signup);
+
+router.put('/profile/:index', ctrl.users.editProfile);
+
+router.delete('/:index', ctrl.users.deleteUser);
+
+module.exports = router;
