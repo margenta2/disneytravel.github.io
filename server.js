8708d78e65cd73d22782a473017263f3417dce2e
Margie Ball <margie_ball@glic.com>
2021-05-25 18:59:44 -0400

    added index controller

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const routes = require('./routes');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(methodOverride('_method'));
 
+app.use('/parks', routes.parks);
+app.use('/users', routes.users);
+app.use('/hotels', routes.hotels);
+
 app.listen(3000, (req, res) => {
     console.log("Listening!");
 })
