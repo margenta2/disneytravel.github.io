c03bb770409016e4a4bcd9a250708f7bdf350a4e
Margie Ball <margie_ball@glic.com>
2021-05-25 19:25:59 -0400

    built out user controller

---
+++ controllers/user.js
@@ -0,0 +1,72 @@
+const User = require('../models').User;
+
+const index = (req, res) => {
+    res.render('users/index.ejs')
+}
+
+const renderSignup = (req, res) => {
+    res.render('users/signup.ejs')
+}
+
+const signup = (req, res) => {
+    User.create(req.body)
+    .then(newUser => {
+        res.redirect(`users/profile/${newUser.id}`);
+    })
+}
+
+const renderLogin = (req, res) => {
+    res.render('users/login.ejs')
+}
+
+const login = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    })
+    .then(foundUser => {
+        res.redirect(`/users/profile/${foundUser.id}`);
+    })
+}
+
+const renderProfile = (req,res) => {
+    User.findByPk(req.params.index)
+    .then(userProfile => {
+        res.render('users/profile.ejs', {
+            user: userProfile
+        })
+    })
+}
+
+const editProfile = (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.index
+        },
+        returning: true
+    })
+    .then(updatedUser => {
+        res.redirect(`/users/profile/${req.params.index}`);
+    })
+}
+
+const deleteUser = (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.index
+        }
+    })
+}
+
+module.exports = {
+    index,
+    renderSignup,
+    renderLogin,
+    signup,
+    login,
+    renderProfile,
+    editProfile,
+    deleteUser
+}
